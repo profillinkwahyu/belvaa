3f71b11ef697dec3cf5adc24e11579214e2899b0
tama <hiwahyu99@gmail.com>
2026-02-11 20:08:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const img = document.querySelector("img");
+  const msg = document.createElement("p");
+  msg.textContent = "Aku bikin ini sambil mikirin kamu.";
+  msg.style.display = "none";
+  msg.style.fontSize = "18px";
+  msg.style.marginTop = "12px";
+
+  document.body.appendChild(msg);
+
   img.addEventListener("click", () => {
-    alert("Aku bikin ini sambil mikirin kamu.");
+    msg.style.display = "block";
   });
 });
-
