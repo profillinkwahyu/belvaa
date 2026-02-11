7c01ba6c482f7dccc6e4795264a5e7ebb3d4f77f
tama <hiwahyu99@gmail.com>
2026-02-11 19:28:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.querySelector("img");
+  img.addEventListener("click", () => {
+    alert("Aku bikin ini sambil mikirin kamu.");
+  });
+});
 
